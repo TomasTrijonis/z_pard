2d8aadfa91cb642a130574ecc43b10a90b1b2a31
TomasTrijonis <tomas.trijonis@gmail.com>
2017-10-21 13:05:34 +0300

    wtf random fix

--- index.php
+++ index.php
@@ -3,15 +3,9 @@ include 'core/init.php';
 include 'includes/overall/header.php'; 
 ?>
 
-<<<<<<< HEAD
 		<h1>Hello</h1>
 		<p>Just a template. Yay.L</p>
-=======
-		<h1>Home</h1>
-		<p>Just a template.</p>
->>>>>>> user-data
-		
+
 		
 <?php include 'includes/overall/footer.php';
 ?>
-<br>
