cc0746d0c3695f467746f7b68bd87e4ce3ee901a
TomasTrijonis <tomas.trijonis@gmail.com>
2017-10-24 20:06:18 +0300

    Revert "Merge branch 'master' of https://github.com/TomasTrijonis/z_pard"
    This reverts commit e9c0566ae1024710ae32f1b5f02b0fefdae80e36, reversing
    changes made to 1d672155ad7039108dbd58b7ddece64ec2ce1907.

--- admin.php
+++ admin.php
@@ -3,8 +3,8 @@ include 'core/init.php';
 admin_protect();
 include 'includes/overall/header.php'; 
 
-$query2 = "SELECT `zanras` FROM `kategorijos`";
-$result2 = $con->query($query2);
+$query2 = "SELECT * FROM `kategorijos`";
+$result2 = mysqli_query($con, $query2);
 ?>
 
 <h1> Admin page </h1>
@@ -31,27 +31,20 @@ include 'core/database/showfiles.php';
 <br>
 <h3>Įterpkite naują prekę:</h3>
 <br>
-<form action="insert.php" method="POST" enctype="multipart/form-data"> 
+<form action="insert.php" method="POST"> 
 Žaidimo pavadinimas: <input type="text" name="PAV" /> <br><br>
 Kūrėjų studija: <input type="text" name="DEV" /> <br><br>
-
 Žanras/Kategorija:
 <select name="kategorijos"> 
 <?php 
    while($row = mysqli_fetch_array($result2)) {
-	$selected = (isset($_POST["GENRE"]) && $_POST["GENRE"] == $row['zanras']) ? 'selected="selected"' : '';
-	?><option value="<?php echo $row['zanras']; ?>" <?php echo $selected;?>><?php echo $row['zanras']; ?></option>
-	
-   <?php
-   //echo "<option value= '".$row['zanras']."' >'".$row['zanras']."'</option>";
-   //antras variantas
+  //   $selected = (isset($_POST["GENRE"]) && $_POST["GENRE"] == $row['zanras']) ? 'selected="selected"' : 'no';
+	echo "<option value= '".$row['zanras']. "'>".$row['zanras']."</option>";
    }
 ?>
 </select> <br><br>
-
 Kaina (€): <input type="number" name="PRICE" min="0" step="0.01"/> <br><br>
-Nuotruka: <input type="file" name="fileToUpload" id="fileToUpload"> <br><br>
-<input type="submit" name="submit" ><br><br>
+<input type="submit" /><br><br>
 </form>
 
 <br><br>
