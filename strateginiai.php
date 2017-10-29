2abde091ca4577e6d708750f974099225f53d6c8
dudelapeno <32816853+dudelapeno@users.noreply.github.com>
2017-10-29 12:33:30 +0200

    .

---
+++ strateginiai.php
@@ -0,0 +1,14 @@
+<?php 
+include 'core/init.php'; 
+include 'includes/overall/header.php'; 
+?>
+
+		
+		
+		
+
+		
+<?php 
+	include 'core/database/showfiles.php';
+	include 'includes/overall/footer.php';
+?>
