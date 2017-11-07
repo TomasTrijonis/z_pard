585d215d960deb15146dc6d17c4953459196084b
TomasTrijonis <tomas.trijonis@gmail.com>
2017-11-07 21:35:00 +0200

    Delete Works

--- delete.php
+++ delete.php
@@ -1,12 +1,23 @@
-<?php//including the database connection file
+<?php
+
+//including the database connection file
 include("core/database/connect.php");
  
 //getting id of the data from url
+
 $gameid = $_GET['gameid'];
  
 //deleting the row from table
-$result_zaid = mysqli_query($con, "DELETE FROM 'zaidimai' WHERE gameid='$gameid'");
- die("error");
+$result_zaid = mysqli_query($con, "DELETE FROM zaidimai WHERE gameid='$gameid'");
+  
+
+
+if (mysqli_query($con, $result_zaid)) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . mysqli_error($con);
+}
+	
 //redirecting to the display page (index.php in our case)
 header("Location: edit1.php");
 
--- delete_kat.php
+++ delete_kat.php
@@ -1,23 +1,24 @@
-<?php//including the database connection file
+<?php
+
+//including the database connection file
 include("core/database/connect.php");
  
 //getting id of the data from url
+
 $genre_id = $_GET['genre_id'];
  
 //deleting the row from table
-//$result_kat = mysqli_query($con, "DELETE FROM 'kategorijos' WHERE genre_id=".$genre_id."");
+$result_kat = mysqli_query($con, "DELETE FROM kategorijos WHERE genre_id='$genre_id'");
+  
 
-$result_kat = "DELETE FROM kategorijos WHERE genre_id=".$genre_id."";
 
 if (mysqli_query($con, $result_kat)) {
     echo "Record deleted successfully";
 } else {
     echo "Error deleting record: " . mysqli_error($con);
 }
-
-mysqli_close($con);
-
-
+	
 //redirecting to the display page (index.php in our case)
-header("Location:edit1_kat.php");
+header("Location: edit1_kat.php");
+
 ?>
