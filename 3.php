fc7ab4ec596b2d1c5caa120e1b39f68ce672a226
TomasTrijonis <tomas.trijonis@gmail.com>
2017-11-20 16:57:13 +0200

    sutaisytas showfiles.
    admin ir kategoriju puslapiu isvedime

--- 3.php
+++ 3.php
@@ -23,20 +23,23 @@ include 'includes/overall/header.php';
 					
 					?>
 
-					<figure><?php
-					echo "<img src=".$row['IMAGE']." />";
-					?>
-					<figcaption>
-					<?php
-					echo $row['PAV'],',  ';
-					echo $row['PRICE'],'€';
-					?><br><?php
-					echo $row['zanras'],',  ';
-					?>
-					</figcaption>
-					</figure>
-
-					<br>
+										    
+   
+                <div class="col-sm-4" style="padding:10px;">  
+                     <form method="post" action="index.php?action=add&gameid=<?php echo $row["gameid"]; ?>">  
+                          <div style="border:0px solid #333; background-color:#f1f1f1; border-radius:5px; padding:8px; height: 430px;" align="center">  
+                               <img src="<?php echo $row["IMAGE"]; ?>" class="img-responsive" /><br />  
+                               <h4 class="text-info"><?php echo $row["PAV"]; ?></h4>  
+                               <h4 class="text-danger">$ <?php echo $row["PRICE"]; ?></h4>  
+                               <input type="text" name="quantity" class="form-control" onkeypress="return isNumberKey(event)"  value="1" />  
+                               <input type="hidden" name="hidden_name" value="<?php echo $row["PAV"]; ?>" />  
+                               <input type="hidden" name="hidden_price" value="<?php echo $row["PRICE"]; ?>" />  
+                      
+                          </div>  
+                     </form>  
+                </div>  
+ 
+   
 					
 					<?php
 					
